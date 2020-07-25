d5b7b18131395ad1d2fbe92978e3f751ea7028c0
Empty-stu <Zasxcdfv7673>
2020-07-25 16:03:46 +0300

    Now nodemon executes script. Static assets now moved to public dir and served from there.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const env = require('../.env');
+const path = require('path');
+const projectRoot = process.cwd();
 const mainRouter = require('./routes/mainRoutes');
 const gameRouter = require('./routes/gameRoutes')
 const port = env.PORT;
@@ -7,5 +9,7 @@ const app = express();
 
 app.use('/', mainRouter);
 app.use('/game', gameRouter);
+
+app.use(express.static(path.join(projectRoot, 'public')));
 
 app.listen(port, () => console.log('\x1b[32m%s\x1b[0m', `App started on ${port} port`));
