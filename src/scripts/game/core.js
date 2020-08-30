2b8fe8099abc7a8200cef17aa0e49f0cb12d5121
Empty-stu <Zasxcdfv7673>
2020-08-30 21:41:54 +0300

    Background become static.

--- src/scripts/game/core.js
+++ src/scripts/game/core.js
@@ -1,36 +1,86 @@
-const app = new PIXI.Application({
-    width: 800, height: 600, backgroundColor: 0x1099bb, resolution: 1,
-});
+const starTexture = PIXI.Texture.from('https://pixijs.io/examples/examples/assets/star.png');
 
-document.body.appendChild(app.view);
+const starAmount = 1000;
+let cameraZ = 0;
+const fov = 20;
+const baseSpeed = 0.025;
+let speed = 0;
+const starStretch = 5;
+const starBaseSize = 0.05;
 
-const container = new PIXI.Container();
+const stars = [];
 
-app.stage.addChild(container);
-
-// Create a new texture
-const texture = PIXI.Texture.from('https://pixijs.io/examples/examples/assets/bunny.png');
-
-// Create a 5x5 grid of bunnies
-for (let i = 0; i < 25; i++) {
-    const bunny = new PIXI.Sprite(texture);
-    bunny.anchor.set(0.5);
-    bunny.x = (i % 5) * 40;
-    bunny.y = Math.floor(i / 5) * 40;
-    container.addChild(bunny);
+function getNewApp() {
+    return new PIXI.Application({
+        width: window.innerWidth,
+        height: window.innerHeight,
+        backgroundColor: 0x000000,
+        resolution: 1,
+    });
 }
 
-// Move container to the center
-container.x = app.screen.width / 2;
-container.y = app.screen.height / 2;
+function randomizeStar(star, initial) {
+    star.z = initial ? Math.random() * 2000 : cameraZ + Math.random() * 1000 + 2000;
 
-// Center bunny sprite in local container coordinates
-container.pivot.x = container.width / 2;
-container.pivot.y = container.height / 2;
+    // Calculate star positions with radial random coordinate so no star hits the camera.
+    const deg = Math.random() * Math.PI * 2;
+    const distance = Math.random() * 50 + 1;
+    star.x = Math.cos(deg) * distance;
+    star.y = Math.sin(deg) * distance;
+}
 
-// Listen for animate update
-app.ticker.add((delta) => {
-    // rotate the container!
-    // use delta to create frame-independent transform
-    container.rotation -= 0.01 * delta;
-});
+function getInitializedBackground(app) {
+    const background = new PIXI.Container();
+
+    for (let i = 0; i < starAmount; i++) {
+        const star = {
+            sprite: new PIXI.Sprite(starTexture),
+            z: 0,
+            x: 0,
+            y: 0,
+        };
+        star.sprite.anchor.x = 0.5;
+        star.sprite.anchor.y = 0.7;
+        randomizeStar(star, true);
+        background.addChild(star.sprite);
+        stars.push(star);
+    }
+
+
+    // Listen for animate update
+    app.ticker.add((delta) => {
+        // Simple easing. This should be changed to proper easing function when used for real.
+        // speed += (warpSpeed - speed) / 20;
+        speed += speed / 20;
+        cameraZ += delta * 10 * (speed + baseSpeed);
+        for (let i = 0; i < starAmount; i++) {
+            const star = stars[i];
+            if (star.z < cameraZ) randomizeStar(star);
+
+            // Map star 3d position to 2d with really simple projection
+            const z = star.z - cameraZ;
+            star.sprite.x = star.x * (fov / z) * app.renderer.screen.width + app.renderer.screen.width / 2;
+            star.sprite.y = star.y * (fov / z) * app.renderer.screen.width + app.renderer.screen.height / 2;
+
+            // Calculate star scale & rotation.
+            const dxCenter = star.sprite.x - app.renderer.screen.width / 2;
+            const dyCenter = star.sprite.y - app.renderer.screen.height / 2;
+            const distanceCenter = Math.sqrt(dxCenter * dxCenter + dyCenter * dyCenter);
+            const distanceScale = Math.max(0, (2000 - z) / 2000);
+            star.sprite.scale.x = distanceScale * starBaseSize;
+            // Star is looking towards center so that y axis is towards center.
+            // Scale the star depending on how fast we are moving, what the stretchfactor is and depending on how far away it is from the center.
+            star.sprite.scale.y = distanceScale * starBaseSize + distanceScale * speed * starStretch * distanceCenter / app.renderer.screen.width;
+            star.sprite.rotation = Math.atan2(dyCenter, dxCenter) + Math.PI / 2;
+        }
+    });
+
+    return background;
+}
+
+const app = getNewApp();
+const background = getInitializedBackground(app);
+
+app.stage.addChild(background);
+
+document.body.appendChild(app.view);
